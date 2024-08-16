7b99f5f9bc8e093d6cc0dfafc04bc6d13041164e
Hao Zhong <s3806357@student.rmit.edu.au>
2024-08-16 16:36:21 +1000

    finished week3 all activity

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup>
-import Json from './components/JSON.vue'
+import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
-  <main>
-    <Json />
-  </main>
+  <!-- <main> -->
+  <Form />
+  <!-- </main> -->
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -43,4 +44,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
