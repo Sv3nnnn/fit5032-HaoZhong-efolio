0bd2fcc69b9f95a890b3731750b3a2ef70767dfa
Hao Zhong <s3806357@student.rmit.edu.au>
2024-08-16 16:54:13 +1000

    add validation for week4 activity 2

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -161,11 +161,24 @@ const submitForm = () => {
     <form @submit.prevent="submitForm">
       <div class="mb-3">
         <label for="username" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" v-model="formData.username" />
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          required
+          v-model="formData.username"
+        />
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" v-model="formData.password" />
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          minlength="4"
+          maxlength="10"
+          v-model="formData.password"
+        />
       </div>
       <div class="mb-3">
         <div class="form-check">
@@ -173,6 +186,7 @@ const submitForm = () => {
             type="checkbox"
             class="form-check-input"
             id="isAustralian"
+            required
             v-model="formData.isAustralian"
           />
           <label class="form-check-label" for="isAustralian">Australian Resident?</label>
@@ -180,11 +194,18 @@ const submitForm = () => {
       </div>
       <div class="mb-3">
         <label for="reason" class="form-label">Reason for joining</label>
-        <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+        <textarea
+          class="form-control"
+          id="reason"
+          rows="3"
+          minlength="10"
+          maxlength="100"
+          v-model="formData.reason"
+        ></textarea>
       </div>
       <div class="mb-3">
         <label for="gender" class="form-label">Gender</label>
-        <select class="form-select" id="gender" v-model="formData.gender">
+        <select class="form-select" id="gender" required v-model="formData.gender">
           <option value="male">Male</option>
           <option value="female">Female</option>
           <option value="other">Other</option>
