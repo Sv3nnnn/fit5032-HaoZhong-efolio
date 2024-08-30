28bf0be1f5e6d4c259ca2b822c913471fa4b7589
Hao Zhong <s3806357@student.rmit.edu.au>
2024-08-30 20:53:37 +1000

    added local storage

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,19 +11,19 @@
             <div class="col-md-6">
               <label for="username" class="form-label">Username</label>
               <input type="text" class="form-control" id="username" v-model="username" @blur="validateUsername"
-                required />
+                @input="updateLastActivity" required />
               <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-md-6">
               <label for="password" class="form-label">Password</label>
               <input type="password" class="form-control" id="password" v-model="password" @blur="validatePassword"
-                required />
+                @input="updateLastActivity" required />
               <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
           <div class="mb-3">
             <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="rememberMe" v-model="rememberMe" />
+              <input type="checkbox" class="form-check-input" id="rememberMe" v-model="rememberMe" @change="updateLastActivity" />
               <label class="form-check-label" for="rememberMe">Remember me</label>
             </div>
           </div>
@@ -38,12 +38,17 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted, onUnmounted } from 'vue'
 
 const username = ref('')
 const password = ref('')
 const rememberMe = ref(false)
 const errors = ref({ username: '', password: '' })
+const isLoggedIn = ref(false)
+const lastActivity = ref(Date.now())
+
+const LOGOUT_TIMEOUT = 30 * 60 * 1000 
+let logoutTimer
 
 const validateUsername = () => {
   if (username.value.length < 3) {
@@ -81,6 +86,18 @@ const handleLogin = () => {
       password: password.value,
       rememberMe: rememberMe.value
     })
+
+    isLoggedIn.value = true
+    updateLastActivity()
+
+    // Save to localStorage if "Remember me" is checked
+    if (rememberMe.value) {
+      localStorage.setItem('rememberedUsername', username.value)
+      localStorage.setItem('isLoggedIn', 'true')
+      localStorage.setItem('lastActivity', lastActivity.value.toString())
+    }
+    
+    startLogoutTimer()
   } else {
     console.log('Form is not valid. Please correct the errors.')
   }
@@ -91,8 +108,60 @@ const clearForm = () => {
   password.value = ''
   rememberMe.value = false
   errors.value = { username: '', password: '' }
+  localStorage.removeItem('rememberedUsername')
+  localStorage.removeItem('isLoggedIn')
+  localStorage.removeItem('lastActivity')
+  isLoggedIn.value = false
+  clearTimeout(logoutTimer)
 }
-</script>
 
-<style scoped>
-</style>
+const loadRememberedUser = () => {
+  const rememberedUsername = localStorage.getItem('rememberedUsername')
+  const storedIsLoggedIn = localStorage.getItem('isLoggedIn')
+  const storedLastActivity = localStorage.getItem('lastActivity')
+
+  if (rememberedUsername && storedIsLoggedIn === 'true' && storedLastActivity) {
+    const timeSinceLastActivity = Date.now() - parseInt(storedLastActivity)
+    if (timeSinceLastActivity < LOGOUT_TIMEOUT) {
+      username.value = rememberedUsername
+      rememberMe.value = true
+      isLoggedIn.value = true
+      lastActivity.value = parseInt(storedLastActivity)
+      startLogoutTimer()
+    } else {
+      clearForm() 
+    }
+  }
+}
+
+const updateLastActivity = () => {
+  lastActivity.value = Date.now()
+  if (rememberMe.value) {
+    localStorage.setItem('lastActivity', lastActivity.value.toString())
+  }
+}
+
+const startLogoutTimer = () => {
+  clearTimeout(logoutTimer)
+  logoutTimer = setTimeout(autoLogout, LOGOUT_TIMEOUT)
+}
+
+const autoLogout = () => {
+  if (isLoggedIn.value) {
+    console.log('Auto logout due to inactivity')
+    clearForm()
+  }
+}
+
+onMounted(() => {
+  loadRememberedUser()
+  window.addEventListener('mousemove', updateLastActivity)
+  window.addEventListener('keypress', updateLastActivity)
+})
+
+onUnmounted(() => {
+  clearTimeout(logoutTimer)
+  window.removeEventListener('mousemove', updateLastActivity)
+  window.removeEventListener('keypress', updateLastActivity)
+})
+</script>
