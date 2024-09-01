ff87affe50daabf9ac5c2c0ecd27bbd4f6b7ed69
Hao Zhong <s3806357@student.rmit.edu.au>
2024-09-01 22:41:38 +1000

    same

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 // import process from 'process';
-import.meta.env.VITE_FIREBASE_API_KEY;
+// import.meta.env.VITE_FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
