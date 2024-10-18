8275c29cdb52349a12c5f4ddbb2291c6895959ec
Hao Zhong <s3806357@student.rmit.edu.au>
2024-10-18 18:38:16 +1100

    11

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  },
-  base: process.env.NODE_ENV === 'production' ? '/fit5032-HaoZhong-efolio/' : '/'
-
+  base: process.env.NODE_ENV === 'production'
+    ? '/fit5032-HaoZhong-efolio/'  
+    : '/'
 })
