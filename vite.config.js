27f6c3819b9bc7476e6348ad8a817bd00ba2d4ba
Hao Zhong <s3806357@student.rmit.edu.au>
2024-10-18 18:04:13 +1100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,10 +14,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production'
-    ? process.env.VITE_BASE_URL  // 使用.env文件中定义的库名作为base路径
-    : '/',
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  }
-});
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/fit5032-HaoZhong-efolio/'  
+    : '/'
+})
